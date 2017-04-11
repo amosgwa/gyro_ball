f9fe4197436758a8e770ba496d91107b72b78974
Amos Gwa <amosgwa@gmail.com>
2017-04-11 14:14:45 -0600

    Initial commit

---
+++ app/src/main/java/com/csci448/agwa/draganddraw/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.csci448.agwa.draganddraw;
+
+/**
+ * Created by amosgwa on 4/7/17.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
